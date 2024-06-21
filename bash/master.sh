b2a7cd2dc59ffbf74e6db82ccd67b158cfb88c7f
wicaksu <wicaksono.bayuaji.wba@gmail.com>
2024-06-21 20:57:57 +0700

    ss

--- bash/master.sh
+++ bash/master.sh
@@ -2,59 +2,77 @@
 
 set -e  # Memastikan script berhenti jika ada kesalahan
 
-
+# Memperbarui paket dan menginstal Docker
 sudo apt update
-
-sudo systemctl stop apparmor && sudo systemctl disable apparmor
-sudo systemctl restart containerd.service
-
 sudo apt install docker.io -y
 
-sudo systemctl enable docker
+# Menghentikan dan menonaktifkan AppArmor
+sudo systemctl stop apparmor && sudo systemctl disable apparmor
 
+# Mengaktifkan dan memulai Docker
+sudo systemctl enable docker
 sudo systemctl start docker
 
+# Menambahkan kunci GPG untuk repositori Kubernetes
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
+# Menambahkan repositori Kubernetes
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg arch=amd64] https://pkgs.k8s.io/core:/stable:/v1.30/deb/amd64 stable main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+# Memperbarui paket setelah menambahkan repositori baru
 sudo apt update
-
 sudo apt install -y kubelet kubeadm kubectl
-
 sudo apt-mark hold kubelet kubeadm kubectl
 
+# Menonaktifkan swap
 sudo swapoff -a
-
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/' /etc/fstab
 
-sudo nano /etc/modules-load.d/containerd.conf
-echo "overlay" >> /etc/modules-load.d/containerd.conf
-echo "br_netfilter" >> /etc/modules-load.d/containerd.conf
+# Mengonfigurasi modul untuk containerd
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
 
+# Memuat modul kernel
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-sudo nano /etc/sysctl.d/kubernetes.conf
-echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.d/kubernetes.conf
-echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/kubernetes.conf
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/kubernetes.conf
-
+# Mengonfigurasi sysctl untuk Kubernetes
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
 sudo sysctl --system
 
-sudo nano /etc/default/kubelet
-echo 'KUBELET_EXTRA_ARGS="--cgroup-driver=cgroupfs"' >> /etc/default/kubelet
-sudo systemctl daemon-reload && sudo systemctl restart kubelet
+# Mengonfigurasi kubelet
+echo 'KUBELET_EXTRA_ARGS="--cgroup-driver=cgroupfs"' | sudo tee /etc/default/kubelet
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
 
-sudo nano /etc/docker/daemon.json
-echo '{ "exec-opts": ["native.cgroupdriver=systemd"],"log-driver": "json-file","log-opts": {"max-size": "100m"},"storage-driver": "overlay2" }' >> /etc/docker/daemon.json
-sudo systemctl daemon-reload && sudo systemctl restart docker
+# Mengonfigurasi Docker daemon
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {"max-size": "100m"},
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
+# Restart containerd
+sudo systemctl restart containerd.service
+
+# Inisialisasi Kubernetes
 sudo kubeadm init --control-plane-endpoint="$1" --upload-certs
 
+# Mengonfigurasi kubectl
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-
+# Menampilkan node
 kubectl get nodes
--- bash/worker.sh
+++ bash/worker.sh
@@ -2,28 +2,28 @@
 
 set -e  # Memastikan script berhenti jika ada kesalahan
 
-
+# Memperbarui daftar paket dan menginstal docker
 sudo apt update
-
-sudo systemctl stop apparmor && sudo systemctl disable apparmor
-sudo systemctl restart containerd.service
-
 sudo apt install docker.io -y
 
-sudo systemctl enable docker
+# Menghentikan dan menonaktifkan apparmor, lalu me-restart containerd
+sudo systemctl stop apparmor && sudo systemctl disable apparmor
 
+# Mengaktifkan dan memulai layanan docker
+sudo systemctl enable docker
 sudo systemctl start docker
 
+# Menambahkan kunci GPG untuk repositori Kubernetes
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
+# Menambahkan repositori Kubernetes ke daftar sumber APT
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg arch=amd64] https://pkgs.k8s.io/core:/stable:/v1.30/deb/amd64 stable main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+# Memperbarui daftar paket setelah menambahkan repositori baru dan menginstal kubelet, kubeadm, dan kubectl
 sudo apt update
-
 sudo apt install -y kubelet kubeadm kubectl
-
 sudo apt-mark hold kubelet kubeadm kubectl
 
+# Menonaktifkan swap dan mengomentari baris swap di fstab
 sudo swapoff -a
-
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/' /etc/fstab
